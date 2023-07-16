d5b34cee78a40a427b777da77d8ee8c5f2ee93dc
TCHAKAL <ahmedtchakal@gmail.com>
2023-07-16 17:16:43 +0200

    Fisrt and last commit

---
+++ src/test/java/com/tchakal/keyclock/KeyclockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tchakal.keyclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyclockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
